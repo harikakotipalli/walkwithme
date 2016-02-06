b59171f3ba82e0808937c8bc0063ffe0adcc47c7
Alex Pereira <alex.pereira@outlook.com>
2016-02-06 18:31:51 -0500

    gooo

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ $(document).ready(function () {
 	var signInPg = $("#signIn");
 	var app= $("#app");
 
-	signInBtn.on('click', function () {
+	signInBtn.on('click', function (e) {
+		e.preventDefault();
 		homePg.hide(300);
 		signInPg.show(800);
 	});
-	signInBtn2.on('click', function () {
+	signInBtn2.on('click', function (e) {
+	 	e.preventDefault();
 		signInPg.hide(300);
 		app.show(800);
 	});
