1cb1de574477d959712775c76a0dd4dc5fa9fcd9
Alex Pereira <alex.pereira@outlook.com>
2016-02-07 12:25:35 -0500

    gooo

--- chat/chat.php
+++ chat/chat.php
@@ -15,7 +15,7 @@
 	<script src="js/readNwrite.js"></script>
 
 </head>
-<body style="background-img:url('/../images/background.jpg')">
+<body>
 	<div id="countdown"><p></p></div>
 	<div class="display"></div>
 	<a class="logout" href="../app.php">GO BACK</a>
